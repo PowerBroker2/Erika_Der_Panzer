ff853a9c32c7427eb5c5afbcf8cb67d569b23201
PB2 <gitstuff2@gmail.com>
2021-04-21 18:57:50 -0400

    Major TX refactor

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,79 +1,96 @@
-#include <SoftwareSerial.h>
+#include "OneButton.h"
+#include "SerialTransfer.h"
+#include "FireTimer.h"
 
 
 
 
-const unsigned long RECOIL_DELAY = 2000;
-const unsigned long DEP_DELAY    = 1;
-
-const int STICK_DEAD_MAX    = 540;
-const int STICK_DEAD_MIN    = 500;
-
-const byte SPEED_MAX        = 180;
-const byte SPEED_MIN        = 0;
-
-const byte TRAV_MAX         = 180;
-const byte TRAV_MID         = 90;
-const byte TRAV_MIN         = 0;
-
-const byte STICK_OFFSET_MAX = 90;
-const byte STICK_OFFSET_MIN = 0;
-
-const byte DEP_MAX          = 105;
-const byte DEP_MID          = 45;
-const byte DEP_MIN          = 0;
-const byte DEP_DELTA        = 1;
-
-const byte START_OF_FRAME   = 200;
-
-const byte X_AXIS_PIN       = 0;
-const byte Y_AXIS_PIN       = 1;
-
-const byte DEP_PIN          = 2;
-const byte TRAV_PIN         = 3;
-const byte TRIG_PIN         = 5;
-const byte SOUND_PIN        = 10;
+#define DEBUG 0
 
 
 
 
-SoftwareSerial BTserial(2, 3); // RX | TX
+const int STICK_DEAD_MAX = 540;
+const int STICK_DEAD_MIN = 490;
+
+const int TRAV_MAX = 180;
+const int TRAV_MID = 90;
+const int TRAV_MIN = 0;
+
+const int RECOIL_DELAY  = 2000;
+const int DEPRESS_DELAY = 85;
+
+const int DEP_MAX = 105;
+const int DEP_MIN = -10;
+
+const int STICK_OFFSET_MAX = 90;
+const int STICK_OFFSET_MIN = 0;
+
+const int SPEED_MAX = 180;
+const int SPEED_MIN = 0;
+
+const int VOL_PIN = A5;
+const int DIF_PIN = A4;
+
+const int X_AXIS_PIN = A0;
+const int Y_AXIS_PIN = A1;
+
+const int DEP_PIN  = A3;
+const int TRAV_PIN = A2;
+
+const int TRIG_PIN  = 4;
+const int SOUND_PIN = 5;
+
+
+
+
+OneButton trigBtn = OneButton(
+  TRIG_PIN,    // Input pin for the button
+  true,        // Button is active LOW
+  true         // Enable internal pull-up resistor
+);
+
+OneButton soundBtn = OneButton(
+  SOUND_PIN,   // Input pin for the button
+  true,        // Button is active LOW
+  true         // Enable internal pull-up resistor
+);
+
+SerialTransfer myTransfer;
+
+FireTimer recoilDelay;
+FireTimer depressDelay;
 
 struct control
 {
-  uint8_t RSpeed;
-  uint8_t LSpeed;
-  int8_t Depression;
-  uint8_t Traverse;
-  uint8_t Fire;
-  uint8_t Volume;
-  uint8_t Sing;
+  int   RSpeed;
+  int   LSpeed;
+  float Depression;
+  int   Traverse;
+  bool  Fire;
+  int   Volume;
+  bool  Sing;
 } Tonk;
 
 
 
 
-byte current_Depression = DEP_MID;
-byte currentTriggerState = 1;
-byte previousTriggerState = 1;
-byte debounce = 0;
-byte pressDetected = 0;
+static void handleTrig()
+{
+  if (!Tonk.Fire)
+  {
+    Tonk.Fire = true;
+    recoilDelay.start();
+  }
+}
 
-byte test = 1;
 
-byte singingCurrentTriggerState = 1;
-byte singingPreviousTriggerState = 1;
-byte singingDebounce = 0;
-byte singingPressDetected = 0;
-byte currentSingStatus = 0;
 
-unsigned long currentTime = millis();
-unsigned long timeBench = currentTime;
 
-unsigned long dep_currentTime = millis();
-unsigned long dep_timeBench = dep_currentTime;
-
-int driveDirection = 1; // 1 - forward
+static void handleSound()
+{
+  Tonk.Sing = !Tonk.Sing;
+}
 
 
 
@@ -81,10 +98,14 @@ int driveDirection = 1; // 1 - forward
 void setup()
 {
   Serial.begin(115200);
-  BTserial.begin(9600);
-
-  pinMode(TRIG_PIN, INPUT_PULLUP);
-  pinMode(SOUND_PIN, INPUT_PULLUP);
+  
+  trigBtn.attachClick(handleTrig);
+  soundBtn.attachClick(handleSound);
+  
+  myTransfer.begin(Serial, false);
+  
+  recoilDelay.begin(RECOIL_DELAY);
+  depressDelay.begin(DEPRESS_DELAY);
 }
 
 
@@ -92,51 +113,84 @@ void setup()
 
 void loop()
 {
+  Buttons();
+  Sound();
+  Turret();
+  Throttle();
+
+#if DEBUG
+  Serial.print("RSpeed:\t\t"); Serial.println(Tonk.RSpeed);
+  Serial.print("LSpeed:\t\t"); Serial.println(Tonk.LSpeed);
+  Serial.print("Depression:\t"); Serial.println(Tonk.Depression);
+  Serial.print("Traverse:\t"); Serial.println(Tonk.Traverse);
+  Serial.print("Fire:\t\t"); Serial.println(Tonk.Fire);
+  Serial.print("Volume:\t\t"); Serial.println(Tonk.Volume);
+  Serial.print("Sing:\t\t"); Serial.println(Tonk.Sing);
+  Serial.println();
+  Serial.println();
+#else
+  myTransfer.sendDatum(Tonk);
+#endif
+}
+
+
+
+
+void Buttons()
+{
+  trigBtn.tick();
+  soundBtn.tick();
+}
+
+
+
+
+void Sound()
+{
+  Tonk.Volume = map(analogRead(VOL_PIN), 0, 1023, 0, 30);
+}
+
+
+
+
+void Turret()
+{
+  if (recoilDelay.fire() && Tonk.Fire)
+    Tonk.Fire = false;
+  
+  int trav = analogRead(TRAV_PIN);
+  
+  if (trav >= STICK_DEAD_MAX)
+    Tonk.Traverse = constrain(map(trav, STICK_DEAD_MAX, 1023, TRAV_MID, TRAV_MIN), TRAV_MIN, TRAV_MID);
+  else if (trav <= STICK_DEAD_MIN)
+    Tonk.Traverse = constrain(map(trav, STICK_DEAD_MIN, 0, TRAV_MID, TRAV_MAX), TRAV_MID, TRAV_MAX);
+  else
+    Tonk.Traverse = TRAV_MID;
+  
+  if (depressDelay.fire())
+  {
+    int depr = analogRead(DEP_PIN);
+    
+    if (depr >= STICK_DEAD_MAX)
+      Tonk.Depression += map(depr, STICK_DEAD_MAX, 1023, 0, 5);
+    else if (depr <= STICK_DEAD_MIN)
+      Tonk.Depression += map(depr, STICK_DEAD_MIN, 0, -5, 0);
+    
+    Tonk.Depression = constrain(Tonk.Depression, DEP_MIN, DEP_MAX);
+  }
+}
+
+
+
+
+void Throttle()
+{
+  static bool driveDirection = true;
+
   int x = analogRead(X_AXIS_PIN);
   int y = analogRead(Y_AXIS_PIN);
-  int depr = analogRead(DEP_PIN);
-  int trav = analogRead(TRAV_PIN);
+  
   uint8_t offset = 0;
-
-  Tonk.Volume = map(analogRead(4), 0, 1023, 0, 30);
-
-  if (Tonk.Fire == 1)
-  {
-    test = 1;
-    currentTime = millis();
-    timeBench = currentTime;
-  }
-  if (test)
-  {
-    currentTime = millis();
-    if (currentTime - timeBench >= RECOIL_DELAY)
-    {
-      timeBench = currentTime;
-      Tonk.Fire = checkFallingEdge();
-      test = 0;
-    }
-    else
-      Tonk.Fire = 0;
-  }
-  else
-    Tonk.Fire = checkFallingEdge();
-
-  if (singingCheckFallingEdge())
-  {
-    if(currentSingStatus)
-      currentSingStatus = 0;
-    else
-      currentSingStatus = 1;
-  }
-  Tonk.Sing = currentSingStatus;
-
-  dep_currentTime = millis();
-  if (dep_currentTime - dep_timeBench > DEP_DELAY)
-  {
-    dep_timeBench = dep_currentTime;
-    add_Depression(depr);
-    Tonk.Depression = current_Depression;
-  }
 
   if (y >= STICK_DEAD_MAX)
   {
@@ -150,157 +204,40 @@ void loop()
   }
   else
     offset = 0;
-
-  Throttle(x, offset, &Tonk.RSpeed, &Tonk.LSpeed);
-
-  if (trav >= STICK_DEAD_MAX)
-    Tonk.Traverse = map(trav, STICK_DEAD_MAX, 1023, TRAV_MID, TRAV_MIN);
-  else if (trav <= STICK_DEAD_MIN)
-    Tonk.Traverse = map(trav, STICK_DEAD_MIN, 0, TRAV_MID, TRAV_MAX);
-  else
-    Tonk.Traverse = TRAV_MID;
-
-  BTserial.write(START_OF_FRAME);
-  BTserial.write(Tonk.Fire);
-  BTserial.write(Tonk.LSpeed);
-  BTserial.write(Tonk.RSpeed);
-  BTserial.write(Tonk.Depression);
-  BTserial.write(Tonk.Traverse);
-  BTserial.write(Tonk.Volume);
-  BTserial.write(Tonk.Sing);
-
-  delay(100);
-}
-
-
-
-void Throttle(int x, uint8_t offset, uint8_t * right_Speed, uint8_t * left_Speed)
-{
+  
   if (x >= STICK_DEAD_MAX)
   {
-    *right_Speed = 90;
-    *left_Speed = map(x, STICK_DEAD_MAX, 1023, 90, SPEED_MIN);
+    Tonk.RSpeed = 90;
+    Tonk.LSpeed = map(x, STICK_DEAD_MAX, 1023, 90, SPEED_MIN);
   }
   else if (x <= STICK_DEAD_MIN)
   {
-    *left_Speed = 90;
-    *right_Speed = map(x, STICK_DEAD_MAX, 0, 90, SPEED_MAX);
+    Tonk.LSpeed = 90;
+    Tonk.RSpeed = map(x, STICK_DEAD_MAX, 0, 90, SPEED_MAX);
   }
   else
   {
-    *right_Speed = 90;
-    *left_Speed = 90;
+    Tonk.RSpeed = 90;
+    Tonk.LSpeed = 90;
   }
 
-  if ((offset > 0) && (driveDirection == 0))
+  if ((offset > 0) && !driveDirection)
   {
-    *right_Speed = *right_Speed - offset;
-    *left_Speed = *left_Speed + offset;
+    Tonk.RSpeed -= offset;
+    Tonk.LSpeed += offset;
   }
-  else if ((offset > 0) && (driveDirection == 1))
+  else if ((offset > 0) && driveDirection)
   {
-    *right_Speed = *right_Speed + offset;
-    *left_Speed = *left_Speed - offset;
-  }
-  else
-  {
-    //do nothing
+    Tonk.RSpeed += offset;
+    Tonk.LSpeed -= offset;
   }
 
-  if (*right_Speed > SPEED_MAX)
-    *right_Speed = SPEED_MAX;
-  if (*left_Speed > SPEED_MAX)
-    *left_Speed = SPEED_MAX;
-  if (*right_Speed < SPEED_MIN)
-    *right_Speed = SPEED_MIN;
-  if (*left_Speed < SPEED_MIN)
-    *left_Speed = SPEED_MIN;
+  if (Tonk.RSpeed > SPEED_MAX)
+    Tonk.RSpeed = SPEED_MAX;
+  if (Tonk.LSpeed > SPEED_MAX)
+    Tonk.LSpeed = SPEED_MAX;
+  if (Tonk.RSpeed < SPEED_MIN)
+    Tonk.RSpeed = SPEED_MIN;
+  if (Tonk.LSpeed < SPEED_MIN)
+    Tonk.LSpeed = SPEED_MIN;
 }
-
-
-
-
-void add_Depression(int depr)
-{
-  if ((depr >= STICK_DEAD_MAX) && ((current_Depression - DEP_DELTA) >= DEP_MIN) && (current_Depression > DEP_DELTA))
-    current_Depression -= DEP_DELTA;
-  else if ((depr <= STICK_DEAD_MIN) && ((current_Depression + DEP_DELTA) <= DEP_MAX))
-    current_Depression += DEP_DELTA;
-}
-
-
-
-
-uint8_t checkFallingEdge()
-{
-  uint8_t fire = 0;
-
-  //prime
-  previousTriggerState = currentTriggerState;
-  
-  //poll button
-  if (digitalRead(TRIG_PIN))
-    currentTriggerState = 1;
-  else
-    currentTriggerState = 0;
-
-  //check for falling edge
-  if ((currentTriggerState == 0) && (previousTriggerState == 1))
-    pressDetected = 1;
-  else
-    pressDetected = 0;
-
-  //debounce after press
-  if (pressDetected)
-  {
-    if (digitalRead(TRIG_PIN))
-      debounce = (debounce << 1) | 1;
-    else
-      debounce = (debounce << 1) & 0xFE;
-
-    if (!debounce)
-      fire = 1;
-  }
-
-  return fire;
-}
-
-
-
-
-uint8_t singingCheckFallingEdge()
-{
-  uint8_t singing = 0;
-
-  //prime
-  singingPreviousTriggerState = singingCurrentTriggerState;
-  
-  //poll button
-  if (digitalRead(SOUND_PIN))
-    singingCurrentTriggerState = 1;
-  else
-    singingCurrentTriggerState = 0;
-
-  //check for falling edge
-  if ((singingCurrentTriggerState == 0) && (singingPreviousTriggerState == 1))
-    singingPressDetected = 1;
-  else
-    singingPressDetected = 0;
-
-  //debounce after press
-  if (singingPressDetected)
-  {
-    if (digitalRead(SOUND_PIN))
-      singingDebounce = (singingDebounce << 1) | 1;
-    else
-      singingDebounce = (singingDebounce << 1) & 0xFE;
-
-    if (!singingDebounce)
-      singing = 1;
-  }
-
-  return singing;
-}
-
-
-
